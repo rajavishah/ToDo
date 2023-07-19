54bd8bfbb902fbdc891b9ae0fdd0b2ccd37d611b
Rajavi Shah <shah.raja@northeastern.edu>
2023-07-19 17:12:24 +0530

    Add Task

---
+++ todolist/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import { useState } from "react";
+
+function App() {
+
+
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChnage = (event) => {
+    setNewTask(event.target.value);
+  }
+
+  const addTask = () => {
+    const newTodoList = [...todoList, newTask];
+    setTodoList(newTodoList);
+  }
+
+  return (
+    <div className="App">
+      <div className="addTask">
+        <input onChange={handleChnage} />
+        <button onClick={addTask}> Add Task </button>
+      </div>
+      <div className="list">
+        {todoList.map((task) => {
+          return <h1>{task}</h1>
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
